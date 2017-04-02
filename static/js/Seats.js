7dab11dad6e6989422f306459a0af11d0a53cc10
Andrew Tu <drewtu2@yahoo.com>
2017-04-02 13:49:58 -0400

    stuff working

--- static/js/Seats.js
+++ static/js/Seats.js
@@ -1,20 +1,29 @@
 9
 // Globals
-var seat_available = false;
+var seat_available = true;
 
 function initSeats()
 {
-	$.getJSON("http://127.0.0.1:5000/api/v1/seats", function(data) {
-		//seat_available = data[0]['available']}
-		
-		if (seat_available){
-			$('#seatInfo').text("Seat Available");
-		}
-
-		else{
-			$('seatInfo').text("Seat Unavailable");
-		}
-	)
+	console.log("initSeats()")
+	
+	$.getJSON("http://127.0.0.1:5000/api/v1/seats", 
+			function(data) {
+				seat_available = data[0]['available'];
+				setSeatStatus();
+			});
 }
 
+function setSeatStatus(){
+	console.log("Set Seat Status");
+	
+	if (seat_available){
+		$('#seatInfo').text("Seat 1: Available");
+		$('#seatInfo').css('background-color', 'green')
+	}
+
+	else{
+		$('#seatInfo').text("Seat Unavailable");
+		$('#seatInfo').css('background-color', 'red')
+	}
+}
 
--- static/js/Test.js
+++ static/js/Test.js
@@ -27,7 +27,7 @@
  */  
   function initGeolocation() {
 	  console.log("Hello world");	
-	  
+	  setSeatStatus();
 	  $.getJSON("http://127.0.0.1:5000/api/v1/printers", function(data) {
 		    printerStatus = data;
 		});
